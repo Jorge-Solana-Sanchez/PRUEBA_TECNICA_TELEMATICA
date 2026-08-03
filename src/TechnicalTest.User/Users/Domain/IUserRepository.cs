7f3e8cecabb24db3d339c43ce5865c5c4fbb75bc
Jorge Solana Sanchez <jorge.solana@cojali.com>
2026-08-03 07:57:28 +0200

    feat: add User domain entity and IUserRepository

---
+++ src/TechnicalTest.User/Users/Domain/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TechnicalTest.User.Users.Domain;
+
+public interface IUserRepository
+{
+    Task<IEnumerable<User>> GetAllAsync();
+    Task<User?> GetByIdAsync(Guid id);
+    Task AddAsync(User user);
+    Task UpdateAsync(User user);
+}
---
+++ src/TechnicalTest.User/Users/Domain/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TechnicalTest.User.Users.Domain;
+
+public class User
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Email { get; private set; }
+    public DateTime Created { get; private set; }
+
+    public User(string name, string email)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Email = email;
+        Created = DateTime.UtcNow;
+    }
+    
+    
+    
+    
+}
